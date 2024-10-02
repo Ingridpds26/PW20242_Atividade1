fe39d45d2cb1679c786e7495a808506bf47eb31d
Ingridpds26 <ingridpds26@gmail.com>
2024-10-01 21:52:57 -0300

    Ingrid

--- main.py
+++ main.py
@@ -17,5 +17,22 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def paginainicial(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
+
+@app.get("/cadastro")
+def get_contato(request: Request):
+     return templates.TemplateResponse("cadastro.html", {"request": request})
+
+@app.post("/post_cadastro")
+def post_cadastro(
+    request: Request, 
+    nome: str = Form(...), 
+    descricao: str = Form(...), 
+    estoque: str = Form(...), 
+    preco: str = Form(...)):
+    
+
+    return RedirectResponse("/", 303)
+
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
